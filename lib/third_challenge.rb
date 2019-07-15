39c57d4bb660f63d6d0108e0be0c7565eb15cb9d
ryanthornton628 <ryanthornton628@gmail.com>
2019-07-15 22:32:46 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -24,11 +24,14 @@ def third_challenge
         },
       :matriarch => {
         :name => "Lady Capulet",
-        
+        :age => "51"
       }
         },
       :heroine => {
-        #your key/value pairs here
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
+      }
         },
       :heroine_friends => []
    }
