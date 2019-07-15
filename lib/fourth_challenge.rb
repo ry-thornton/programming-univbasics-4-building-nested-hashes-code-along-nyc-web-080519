8732438a83c729dd085f68f3883e4d124348f1fe
ryanthornton628 <ryanthornton628@gmail.com>
2019-07-15 22:49:23 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -22,6 +22,11 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
+        {
+          :name => "Nurse",
+          :age => "44",
+          :attitude => "worried"
+        },
       
       ]
    }
