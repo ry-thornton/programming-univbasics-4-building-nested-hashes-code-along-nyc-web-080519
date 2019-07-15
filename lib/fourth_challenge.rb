8fe90912e185c04fadf67aecb04b6b7892d7bad5
ryanthornton628 <ryanthornton628@gmail.com>
2019-07-15 22:54:54 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -1,6 +1,6 @@
 def fourth_challenge
   epic_tragedy = {
-   :monague => {
+   :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
@@ -16,7 +16,7 @@ def fourth_challenge
           :attitude => "hot-headed"
         }
       ]
-   }, 
+   }
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
